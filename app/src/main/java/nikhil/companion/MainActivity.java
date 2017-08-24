72bd7e197c8988e19c76397f6c5c1c7b35339337
gadeelanikhil <30790574+gadeelanikhil@users.noreply.github.com>
2017-08-24 11:47:28 +0530

    Commit
    Here a Edit is done by  adding a comment

--- app/src/main/java/nikhil/companion/MainActivity.java
+++ app/src/main/java/nikhil/companion/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+// its just a edit //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
